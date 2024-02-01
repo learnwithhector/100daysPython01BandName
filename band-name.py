e20bb9c3fe2bdcd29fddcc1d35543511e0f33dea
Hector Macafonty <learnwithhector@gmail.com>
2024-02-01 21:14:11 +0000

    Version 2 adds a randomly generated computer band name from two lists in data.py and displays this to the user.

--- band-name.py
+++ band-name.py
@@ -1,6 +1,13 @@
+from data import city_names, names
+import random
+
+computer_place = random.choice(city_names)
+computer_name = random.choice(names)
+
 print("Welcome to the Band Name Generator.")
 print("Enter the name of a city, town or village: ")
 place = input()
 print("Enter the name of your pet, real or imagined: ")
 pet = input()
 print("Your band name could be " + place + " " + pet)
+print(f"My band is called {computer_place} {computer_name}!")
